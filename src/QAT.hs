8cf9bb8a33ca55f0d50f9c85e3a639083ab27f4d
Wojciech Baranowski <w.baranowski@o2.pl>
2013-11-06 21:28:01 +0100

    QAT: deriving Show

--- src/QAT.hs
+++ src/QAT.hs
@@ -2,15 +2,20 @@ module QAT where
 
 data QAT =
     QAT [Qsel] (Maybe Qexpr) [Qorder]
+    deriving (Show)
 
 data Qsel =
     Qsel Qexpr (Maybe String)
+    deriving (Show)
 
 data Qorder =
     Qorder Qexpr Order NullOrder
+    deriving (Show)
 
 data Order = Oasc | Odesc
+    deriving (Show)
 data NullOrder = Onfirst | Onlast
+    deriving (Show)
 
 data Qexpr = Eor [Qexpr]
            | Eand [Qexpr]
@@ -31,13 +36,17 @@ data Qexpr = Eor [Qexpr]
            | Esquare
            | Evar String
            | Eapp String [Qexpr]
+    deriving (Show)
 
 data MulOper = OpMul | OpDiv | OpMod
+    deriving (Show)
 data AddOper = OpAdd | OpSub
+    deriving (Show)
 data CmpRel = Rle
             | Rlt
             | Rge
             | Rgt
             | Req
             | Rne
+    deriving (Show)
 
