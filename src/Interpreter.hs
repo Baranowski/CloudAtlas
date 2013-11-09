c69ac88b6b7bcb86cc530082bc1d8ba566f30411
Wojciech Baranowski <w.baranowski@o2.pl>
2013-11-09 18:56:24 +0100

    pred - naszkicowane

--- src/Interpreter.hs
+++ src/Interpreter.hs
@@ -13,12 +13,24 @@ performQueries z@(Zone attribs children) = do
     let updatedAttrs = (M.fromList newAttrs) `M.union` attribs
     return $ Zone updatedAttrs newKids
 
-performQ :: QAT -> ReaderT [Zone] (Either String [(String, Attribute)])
+type Interpretation res = ReaderT [Zone] (Either String res)
+
+pred :: Qexpr -> Zone -> Interpretation Bool
+pred e z = do
+    val <- eval [z] e
+    case val of
+        [Abool (Just x)] -> return x
+        [Abool Nothing] -> return False
+        [x] -> Left $ "Expected boolean value, got " ++ (printAType x)
+        _ -> Left "Expected single value"
+
+performQ :: QAT -> [(String, Attribute)]
 performQ (QAT sels wh ordering) = do
+    let rordering = reverse ordering
     let wh = fromMaybe Etrue wh
     origKids <- ask
     kids <- filterM (pred wh) origKids
-    kids <- sortByM (cmp ordering) kids
+    kids <- sortByM (cmp rordering) kids
     mapM evalSel kids sels
     where
         evalSel kids (Qsel e n) = do
