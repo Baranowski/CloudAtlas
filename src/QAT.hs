3449ee5a4aeb4131191375999e08edb5feb33db5
Wojciech Baranowski <w.baranowski@o2.pl>
2013-12-02 14:31:31 +0100

    QAT: show

--- src/QAT.hs
+++ src/QAT.hs
@@ -1,25 +1,54 @@
+{-# LANGUAGE FlexibleInstances #-}
 module QAT where
+
+import Data.List
 
 data QAT =
     QAT [Qsel] (Maybe Qexpr) [Qorder]
-    deriving (Show,Eq)
+    deriving (Eq)
+instance Show QAT where
+    show (QAT sels mbWhere orders) =
+        "SELECT " ++ (concat $ intersperse ", " (map show sels)) ++
+            " " ++ (whereClause mbWhere) ++
+            " " ++ (orderClause orders)
+
+whereClause Nothing = ""
+whereClause (Just e) = "WHERE " ++ (show e)
+orderClause [] = ""
+orderClause xs = "ORDER BY " ++ (concat $ intersperse ", " (map show xs))
 
 data Qnested =
     Qnested Qexpr (Maybe Qexpr) [Qorder]
-    deriving (Show,Eq)
+    deriving (Eq)
+instance Show Qnested where
+    show (Qnested e mbWhere orders) =
+        "SELECT " ++ (show e) ++
+            " " ++ (whereClause mbWhere) ++
+            " " ++ (orderClause orders)
 
 data Qsel =
     Qsel Qexpr String
-    deriving (Show,Eq)
+    deriving (Eq)
+instance Show Qsel where
+    show (Qsel e s) = "(" ++ (show e) ++ ") AS " ++ s
 
 data Qorder =
     Qorder Qexpr Order NullOrder
-    deriving (Show,Eq)
+    deriving (Eq)
+instance Show Qorder where
+    show (Qorder e ord nord) = (show e) ++ " " ++ (show ord) ++ " " ++ (show nord)
 
 data Order = Oasc | Odesc
-    deriving (Show,Eq)
+    deriving (Eq)
+instance Show Order where
+    show Oasc = "ASC"
+    show Odesc = "DESC"
+
 data NullOrder = Onfirst | Onlast
-    deriving (Show,Eq)
+    deriving (Eq)
+instance Show NullOrder where
+    show Onfirst = "NULLS FIRST"
+    show Onlast = "NULLS LAST"
 
 data Qexpr = Eor [Qexpr]
            | Eand [Qexpr]
@@ -37,16 +66,51 @@ data Qexpr = Eor [Qexpr]
            | Eint Int
            | Evar String
            | Eapp String [Qexpr]
-    deriving (Show,Eq)
+    deriving (Eq)
+instance Show Qexpr where
+    show (Eor es) = "(" ++ (concat $ intersperse " OR " (map show es)) ++ ")"
+    show (Eand es) = "(" ++ (concat $ intersperse " AND " (map show es)) ++ ")"
+    show (Enot e) = "NOT " ++ show e
+    show (Erel r e1 e2) = (show e1) ++ " " ++ (show r) ++ " " ++ (show e2)
+    show (Erexp e s) = (show e) ++ " REGEXP \"" ++ s ++ "\""
+    show (Eadd e l) = "(" ++ (show e) ++ (concatMap mulShow l) ++ ")"
+    show (Emul e l) = "(" ++ (show e) ++ (concatMap addShow l) ++ ")"
+    show (Eneg e) = "-" ++ (show e)
+    show Etrue = "TRUE"
+    show Efalse = "FALSE"
+    show (Equery q)= "(" ++ (show q) ++ ")"
+    show (Estr s) = "\"" ++ s ++ "\""
+    show (Efloat d) = show d
+    show (Eint i) = show i
+    show (Evar s) = s
+    show (Eapp s es) = s ++ "(" ++ (concat $ intersperse ", " (map show es)) ++ ")"
+
+mulShow (m, e) = " " ++ (show m) ++ " " ++ (show e)
+addShow (m, e) = " " ++ (show m) ++ " " ++ (show e)
 
 data MulOper = OpMul | OpDiv | OpMod
-    deriving (Show,Eq)
+    deriving (Eq)
+instance Show MulOper where
+    show OpMul = "*"
+    show OpDiv = "/"
+    show OpMod = "%"
 data AddOper = OpAdd | OpSub
-    deriving (Show,Eq)
+    deriving (Eq)
+instance Show AddOper where
+    show OpAdd = "+"
+    show OpSub = "-"
+
 data CmpRel = Rle
             | Rlt
             | Rge
             | Rgt
             | Req
             | Rne
-    deriving (Show,Eq)
+    deriving (Eq)
+instance Show CmpRel where
+    show Rle = "<="
+    show Rlt = "<"
+    show Rge = ">="
+    show Rgt = ">"
+    show Req = "="
+    show Rne = "!="
