efd99bbeba0e10d41199bbe2c9b6c9fe8beb2c19
Wojciech Baranowski <w.baranowski@o2.pl>
2013-12-02 13:49:21 +0100

    Communication: Rozpierducha z deserialize
    kompiluje sie

--- src/Communication.hs
+++ src/Communication.hs
@@ -21,8 +21,30 @@ chToW :: Char -> Word8
 chToW ch = fromIntegral $ ord ch
 
 
+type CommMonad res = (ErrorT String Identity) res
+generalizeId m = return (runIdentity m)
+
 class Serializable a where
     serialize :: a -> [Word8]
+    deserialize :: [Word8] -> CommMonad (a, [Word8])
+
+instance Serializable a => Serializable [a] where
+    serialize xs = (serialize $ length xs) ++ (concatMap serialize xs)
+    deserialize xs = do
+        (i::Int, rest) <- deserialize xs
+        go i [] rest
+        where
+          go 0 acc rest = return (reverse acc, rest)
+          go i acc rest = do
+            (next::a, rest) <- deserialize rest
+            go (i-1) (next:acc) rest
+
+instance (Serializable a, Serializable b) => Serializable (a, b) where
+    serialize (pA, pB) = (serialize pA) ++ (serialize pB)
+    deserialize xs = do
+        (pA, rest) <- deserialize xs
+        (pB, rest) <- deserialize rest
+        return ((pA, pB), rest)
 
 newtype Header = Header {hd_port :: PortNumber}
 
@@ -34,7 +56,7 @@ updateClient _ _ =
     fail "updateClient: Unsupported address"
 
 readHeader msg = do
-    (p::Integer, newMsg) <- desrIntg msg
+    (p::Integer, newMsg) <- deserialize msg
     return (Header (fromIntegral p), newMsg)
     
 data Msg
@@ -52,58 +74,57 @@ instance Serializable Msg where
         sAttrs = (serialize $ length l) ++ (concatMap serAttr l)
         serAttr (n, a) = (serialize n) ++ (serialize a)
 
-generalizeId m = return (runIdentity m)
-desrMsg ::  [Word8] -> (ErrorT String Identity) Msg
-desrMsg (1:xs) = do
-    fr <- desrFreshness xs
-    return $ FreshnessInit fr
-desrMsg (2:xs) = do
-    fr <- desrFreshness xs
-    return $ FreshnessResponse fr
-desrMsg (3:xs) = do
-    rmi <- desrRmi xs
-    return $ RmiReq rmi
-desrMsg _ = fail "desrMsg: Unknown message type"
-deserializeMsg = desrMsg
+    deserialize (1:xs) = do
+        (fr, rest) <- deserialize xs
+        return (FreshnessInit fr, rest)
+    deserialize (2:xs) = do
+        (fr, rest) <- deserialize xs
+        return (FreshnessResponse fr, rest)
+    deserialize (3:xs) = do
+        (rmi, rest) <- desrRmi xs
+        return (RmiReq rmi, rest)
+    deserialize _ = fail "deserialize: Unknown message type"
+deserializeMsg = deserialize :: [Word8] -> CommMonad (Msg, [Word8])
 
 instance Serializable Attribute where
+
     serialize a = [1] -- TODO
 
 newtype Freshness = Freshness [(String, Integer)]
 instance Serializable Freshness where
-    serialize (Freshness l) = (fromIntegral $ length l):(concatMap srSingle l)
-      where
-      srSingle (name, timestamp) = (serialize name) ++ (serialize timestamp)
-desrFreshness (count:xs) = go count [] xs
-  where
-    go 0 acc [] = return $ Freshness $ reverse acc
-    go c acc xs = do
-        (name, remaining) <- desrStr xs
-        (timestamp, remaining) <- desrIntg remaining
-        go (c-1) ((name, timestamp):acc) remaining
+    serialize (Freshness l) = serialize l
 
-instance Serializable String where
-    serialize s = (fromIntegral $ length s):(strToB s)
-desrStr (count:xs) = do
-    let c = fromIntegral count
-    return $ (bToStr (take c xs), drop c xs)
+    deserialize xs = do
+        (l, xs) <- deserialize xs
+        return (Freshness l, xs)
 
 instance Serializable Integer where
     serialize x = go [] x 0
       where
         go acc 0 cells = cells:acc
         go acc x cells = go ((fromIntegral x):acc) (x `shiftR` 8) (cells+1) -- TODO: liczby ujemne
-desrIntg (count:xs) = go (fromIntegral count) 0 xs
-  where
-    go 0 acc xs = return (acc, xs)
-    go c acc (x:xs) =
-        go (c-1)
-           ((acc `shiftL` 8) + (fromIntegral x))
-           xs
+
+    deserialize (count:xs) = go (fromIntegral count) 0 xs
+      where
+        go 0 acc xs = return (acc, xs)
+        go c acc (x:xs) =
+            go (c-1)
+               ((acc `shiftL` 8) + (fromIntegral x))
+               xs
+
 instance Serializable Int where
     serialize i = serialize ((fromIntegral i)::Integer)
+    deserialize xs = do
+        (i::Integer, rest) <- deserialize xs
+        return (fromIntegral i, rest)
+
+instance Serializable Char where
+    serialize ch = [fromIntegral $ ord ch]
+    deserialize (x:xs) =
+        return (chr (fromIntegral x), xs)
+    deserialize _ = fail "Trying to deserialize empty input"
 
 data RemoteCall = RC -- TODO
 instance Serializable RemoteCall where
     serialize _ = return 1
-desrRmi xs = return RC
+desrRmi xs = return (RC, [])
